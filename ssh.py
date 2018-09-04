d2b790637f92b7594fe611908a6111b98b74501b
NMorra <42759446+NMorra@users.noreply.github.com>
2018-09-04 18:10:27 -0300

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -7,6 +7,6 @@ cisco_7200 = {
 'password': 'python',
  }
 
-net_connect = ConnectHandler(device_type='cisco_ios', ip='192.168.122.108', use$
+net_connect = ConnectHandler(device_type='cisco_ios', ip=‘192.168.122.108’, username=‘python’, password=‘python’)
 
 net_connect.find_prompt()
