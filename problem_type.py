dc1b5ea8308758e2e30a28703e03c1c68d1c35e5
Alex Ricardo <alexricardodearaujo@gmail.com>
2018-09-05 17:37:19 -0300

    Create problem_type.py

---
+++ problem_type.py
@@ -0,0 +1,16 @@
+problem_type = str(input('Tipo de problema: serv_degr or serv_out ')) #serv_degr or serv_out
+dmark = str(input('¿equipo de demarcacion? true or false ')) #true or false
+
+if problem_type == 'serv_degr':
+    if dmark == 'true':
+        problem_type = 'serv_degr'
+        dmark = 'true'
+        print('serv_degr_dmark')
+    else:print('serv_degr')
+    
+else:
+    if  dmark == 'true':
+        problem_type = 'serv_out'
+        dmark = 'true'
+        print('serv_out_dmark')
+    else:print('serv_out')
