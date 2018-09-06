1fa080b63437677b597a603f54e6adb01bd1af62
Alex Ricardo <alexricardodearaujo@gmail.com>
2018-09-06 20:32:41 -0300

    Update problem_type.py

--- problem_type.py
+++ problem_type.py
@@ -1,5 +1,7 @@
+# Solo una prueba #
 problem_type = str(input('Tipo de problema: serv_degr or serv_out ')) #serv_degr or serv_out
 dmark = str(input('¿equipo de demarcacion? true or false ')) #true or false
+# Solo una prueba #
 
 if problem_type == 'serv_degr':
     if dmark == 'true':
