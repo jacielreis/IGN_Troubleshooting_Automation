d2637ef6c364bc5517715f391b4dc44ab3e0e936
NMorra <42759446+NMorra@users.noreply.github.com>
2018-09-10 21:48:47 -0300

    Add files via upload
    MTRv1.0

---
+++ MTR.py
@@ -0,0 +1,13 @@
+ #from Scratch, By NicoMorra#
+
+host = str(input('host: '))
+
+import os
+os.system("command")
+
+import os 
+os.system("mtr --report > Report.txt --report-cycles 10 " + host)
+
+
+
+
