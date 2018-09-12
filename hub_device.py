f39cc357e5f5530948b66c9fd22f31926df5f23c
Alex Ricardo <alexricardodearaujo@gmail.com>
2018-09-12 17:36:25 -0300

    Update hub_device.py

--- hub_device.py
+++ hub_device.py
@@ -4,13 +4,9 @@ ipaddr_tun = input('Ingresa direccion IP del servicio: ')
 
 if ipaddr_tun[0:6] in '10.1.7':
     ipaddr_hub = '172.21.4.18'  # MIA1-AR3
-    print(ipaddr_hub, ipaddr_tun)
-elif ipaddr_tun in '10.54':
+elif ipaddr_tun[0:5] in '10.54':
     ipaddr_hub = '172.21.4.66'  # BUE3-AR1
-    print(ipaddr_hub)
-elif ipaddr_tun in '10.55':
+elif ipaddr_tun[0:5] in '10.55':
     ipaddr_hub = '172.21.4.33'  # SAO3-AR1
-    print(ipaddr_hub)
 else:
     ipaddr_hub = '172.21.4.113'  # SCL1-AR1
-    print(ipaddr_hub)
